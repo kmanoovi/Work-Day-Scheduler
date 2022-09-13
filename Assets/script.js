3d3bfffe73c09ada3f0fc08babf8181eb622cfef
Katie Mandovi <ktmandovi@MacBook-Pro.local>
2022-09-13 09:46:09 -0400

    added local storage and time blocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,47 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDay);
+
+$(".saveBtn").on("click", function() {
+    var appointment = description.val();
+    var time = attr("id");
+
+    localStorage.setItem(appointment, time);
+})
+
+$("#8a .description").val(localStorage.getItem("8a"));
+$("#9a .description").val(localStorage.getItem("9a"));
+$("#10a .description").val(localStorage.getItem("10a"));
+$("#11a .description").val(localStorage.getItem("11a"));
+$("#12p .description").val(localStorage.getItem("12p"));
+$("#1p .description").val(localStorage.getItem("1p"));
+$("#2p .description").val(localStorage.getItem("2p"));
+$("#3p .description").val(localStorage.getItem("3p"));
+$("#4p .description").val(localStorage.getItem("4p"));
+$("#5p .description").val(localStorage.getItem("5p"));
+$("#6p .description").val(localStorage.getItem("6p"));
+
+function timeTracker() {
+    var currentHour = moment().hour();
+
+    $(".timeblock").each(function() {
+        var timeBlockHour = parseInt(attr("id".split("hour")[0]));
+
+        if (timeBlockHour < currentHour) {
+            addClass("past");
+            removeClass("future");
+            removeClass("present");
+        }
+        else if (timeBlockHour === currentHour) {
+            removeClass("past");
+            addClass("present");
+            removeClass("future");
+        }
+        else {
+            removeClass("past");
+            removeClass("present");
+            addClass("future");
+        }
+    })
+}
+
+timeTracker();
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Work-Day-Scheduler
+
+A Javascript file was created and linked to the HTML file. moment.js was also linked
+to the HTML file.
+
+Next, the current date was entered into Javascript via the moment format. Afterwards,
+everything was centered.
+
+Created time blocks using separate divs for each hour from 8am-6pm. Used <textarea> element as well as form elements
+so the user is able to enter any specific information into their calendar.
+
+Within each div is a save button. I used the <i> element for a save icon and found the 'fa fa-save' code for a simple icon.
+
+In the Javascript file, localStorage is used to save any appointments that
+the user may have written.
+
+A forloop was created so that the webpage knew when to change the color of the time blocks based on the current time.
